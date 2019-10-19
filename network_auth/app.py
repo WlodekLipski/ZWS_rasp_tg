b444e9779c4f4fc70e45626c3e9aa315b0524fd8
KostiukovAleksey <30317283+KostiukovAleksey@users.noreply.github.com>
2019-10-19 09:27:28 +0200

    single web page for entering credentials

---
+++ network_auth/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        subprocess.Popen(['./switch_network.sh %s %s' % (request.form['ssid'], request.form['password'])], shell=True)
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
