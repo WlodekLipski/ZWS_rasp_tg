23b8c1b12c23df0d97253119d9a6864f01953897
Victor Gasiul <wiktorgasiul@gmail.com>
2019-10-26 14:45:02 +0200

    automatic ip

--- ZSW-web-app/downloadChartAsImage.py
+++ ZSW-web-app/downloadChartAsImage.py
@@ -1,10 +1,13 @@
 import sys
 import time
 from selenium import webdriver
+from requests import get
 
+ip = get('https://api.ipify.org').text
 driver = webdriver.Chrome('chromedriver.exe')
-driver.get("http://156.17.169.251:5000/")
+driver.get("http://" + ip +":5000/")
 button = driver.find_element_by_id(sys.argv[1]+"-chart").find_elements_by_class_name("modebar-btn")[0]
 driver.execute_script("arguments[0].click();", button)
 time.sleep(2)
 driver.close()
+
